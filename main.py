ec9c8f03c91c5a7855fd8972f98e82c6ec0c63c9
Jonas <Jonasattlan@gmail.com>
2021-04-27 16:38:30 +0200

    Les coordonnées y ne sont plus inversées

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import pygame
 import random
-
 pygame.init()
 
 (width, height) = (1280,720)
-bg_color = (255,255,255)
-
+bg_color = (0,0,0)
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Algorithm visualizer")
 screen.fill(bg_color)
@@ -16,20 +14,15 @@ class bar:
         self.pos = x
 
     def draw(self):
-        pygame.draw.line(screen, (0,0,0), (self.pos,600), (self.pos,self.val), 10)
+        pygame.draw.line(screen, (255,255,255), (self.pos,700), (self.pos,700-self.val), 10)
 
-    #def move(self):
-
-
-
-bars = []
-pos = 40
+tab = []
+position = 40
 for i in range(100):
-    bars.append(bar(random.randint(0,600), pos))
-    bars[i].draw()
-    pos += 12
-
-
+    b = bar(random.randint(1,500),position)
+    tab.append(b)
+    tab[i].draw()
+    position += 12
 pygame.display.flip()
 
 running = True
@@ -38,4 +31,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                print("Bruh haram")
