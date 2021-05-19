5187e5c1d559300772f8219c766326e4332b9b61
Oumano <76536468+Oumano@users.noreply.github.com>
2021-05-19 10:42:20 +0200

    Update algorithm.py
    Les algos de tri

---
+++ algorithms.py
@@ -0,0 +1,118 @@
+### Tri Fusion ###
+
+def fusion(T1,T2):
+    if T1 == []:
+        return T2
+    if T2 == []:
+        return T1
+    if T1[0] < T2[0]:
+        return [T1[0]] + [fusion(T1[1:], T2)]
+    else:
+        return [T2[0]] + [fusion(T2[1:], T1)]
+
+def TriFusion(Tab):
+    if Tab == []:
+        return Tab
+    else:
+        milieu = len(Tab)//2
+        return fusion(TriFusion(Tab[:milieu]), TriFusion(Tab[milieu:]))
+
+### --- ###
+
+### Tri Rapide ###
+
+def TriRapide(Tab):
+    if Tab == []:
+        return Tab
+    pivot = Tab[0]
+    L1 = [i for i in Tab[1:] if i <= pivot]
+    L2 = [i for i in Tab[1:] if i > pivot]
+    return [TriRapide(L1)] + [pivot] + [TriRapide(L2)]
+
+### --- ###
+
+### Tri par séléction ###
+
+def TriSelection(tab): 
+    for n in range (len(tab)):
+        for v in range (n+1, len(tab)):
+            if tab[n] > tab[v]:
+                tab[n], tab[v] = tab[v], tab[n]
+
+    return tab
+
+### --- ###
+
+### Tri par Insertion ###
+
+def TriInsertion(tab):
+    for i in range(1, len(tab)):
+        valeur = tab[i]
+        pos = i
+        while pos > 0 and tab[pos-1] > valeur:
+            tab[pos] = tab[pos-1]
+            pos = pos - 1
+        tab[pos] = valeur
+    return tab
+
+### --- ###
+
+### Tri à bulle ###
+
+def TriBulle(Tab):
+    for i in range(len(Tab)):
+        for n in range(len(Tab)-1-i):
+            if Tab[n] > Tab[n+1]:
+                Tab[n], Tab[n+1] = Tab[n+1], Tab[n]
+    return Tab
+
+### --- ###
+
+### Tri par base ###
+import math
+
+def sort(array, radix=10):
+    if len(array) == 0:
+        return array
+
+    # Determine minimum and maximum values
+    minValue = array[0]
+    maxValue = array[0]
+    for i in range(1, len(array)):
+        if array[i] < minValue:
+            minValue = array[i]
+        elif array[i] > maxValue:
+            maxValue = array[i]
+
+  # Perform counting sort on each exponent/digit, starting at the least
+  # significant digit
+    exponent = 1
+    while (maxValue - minValue) / exponent >= 1:
+        array = countingSortByDigit(array, radix, exponent, minValue)
+        exponent *= radix
+
+    return array
+
+def countingSortByDigit(array, radix, exponent, minValue):
+    bucketIndex = -1
+    buckets = [0] * radix
+    output = [None] * len(array)
+
+    # Count frequencies
+    for i in range(0, len(array)):
+        bucketIndex = math.floor(((array[i] - minValue) / exponent) % radix)
+        buckets[bucketIndex] += 1
+
+    # Compute cumulates
+    for i in range(1, radix):
+        buckets[i] += buckets[i - 1]
+
+    # Move records
+    for i in range(len(array) - 1, -1, -1):
+        bucketIndex = math.floor(((array[i] - minValue) / exponent) % radix)
+        buckets[bucketIndex] -= 1
+        output[buckets[bucketIndex]] = array[i]
+
+    return output
+
+### --- ###
