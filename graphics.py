a37c80aa0ddbd48fde167c5a72b590c368f3e00d
Oumano <Oumano.Katsukai@gmail.com>
2021-05-19 16:54:52 +0200

    .

--- graphics.py
+++ graphics.py
@@ -1,1 +1,30 @@
 ### test pour le code graphique ###
+import pygame
+import algorithms
+from radom import randint
+pygame.init()
+
+screen = pygame.display.set_mode((720,1080))
+color = {'white': (255, 255, 255),
+        'red': (255, 0, 0)
+        'blue': (0, 255, 0)
+        'green': (0, 0, 255)
+        'black': (0, 0, 0)}
+
+class Stick:
+    def __init__(self, value, pos):
+        self.value = value
+        self.pos = pos
+
+    def draw(self):
+        pygame.draw.rect(screen, (255, 255, 255), (self.pos,700), (self.pos, 700-self.val), 10)
+
+    def move(self, new_pos):
+        self.pos = new_pos
+        self.draw()
+
+barres = []
+base_pos = 50
+for i in range(50):
+    pos = base_pos+(10*i)
+    barre = Stick(10*randint(0,50), pos)
