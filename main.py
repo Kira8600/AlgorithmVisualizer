f5affff5e57028bdf4aac027b5f47e00ca9ce09e
Jonas <Jonasattlan@gmail.com>
2021-05-20 11:22:08 +0200

    Bouton fonctionnel
    Halal python !

--- main.py
+++ main.py
@@ -1,58 +1,59 @@
-import algorithms
 import pygame
 import random
+<<<<<<< HEAD
+=======
 pygame.init()
+# Bonjour Maxime
+>>>>>>> d6372c901abf71efb8104d97dfa21b52cb1a16fa
 
+#Pygame initialization
+pygame.init()
 (width, height) = (1280,720)
 bg_color = (0,0,0)
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Algorithm visualizer")
 screen.fill(bg_color)
 
-class Bar:
+#The bars
+class bar:
     def __init__(self,value,x):
         self.val = value
         self.pos = x
+        self.color = (255,255,255)
 
     def draw(self):
-        pygame.draw.line(screen, (random.randint(0,255),random.randint(0,255),random.randint(0,255)), (self.pos,700), (self.pos,700-self.val), 10)
+        pygame.draw.line(screen, self.color, (self.pos,700), (self.pos,700-self.val), 10)
 
     def move(self,dest):
         self.pos = dest
         self.draw()
     
+#To switch bars
 def bar_switch(b1,b2):
     p1 = b1.pos
     p2 = b2.pos
     b1.move(p2)
     b2.move(p1)
 
-bars = []
-position = 40
-for i in range(100):
-    b = Bar(random.randint(1,500),position)
-    bars.append(b)
-    bars[i].draw()
-    position += 12
+# Tableau contenant les barres
 
 def rand():
-    rand_tab = []
+    tab = []
+    screen.fill(bg_color)
     position = 40
     for i in range(100):
-        b = Bar(random.randint(1,500),position)
-        rand_tab.append(b)
-        rand_tab[i].draw()
+        b = bar(random.randint(1,500),position)
+        tab.append(b)
         position += 12
-
-def bubble(tab):
     for i in range(len(tab)):
-        for n in range(len(tab)-1-i):
-            if tab[n].val > tab[n+1].val:
-                bar_switch(tab[n],tab[n+1])
-    return tab
+        tab[i].draw()
+    pygame.draw.rect(screen, (255,255,255), pygame.Rect(25, 25, 25, 25))
+
+rand()
 
 running = True
 while running:
+    mouse = pygame.mouse.get_pos()
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -60,8 +61,8 @@ while running:
             pygame.quit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                screen.fill(bg_color)
-                bubble(bars)
-            elif event.key == pygame.K_x:
-                screen.fill(bg_color)
                 rand()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if 25 <= mouse[0] <= 50 and 25 <= mouse[1] <= 50:
+                rand()
+                
