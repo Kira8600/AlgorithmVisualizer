23de2056dc3c0aa2178025e43fc982ca2379eab2
Jonas <Jonasattlan@gmail.com>
2021-05-25 21:16:51 +0200

    J'ai presque fini la classe window

--- main.py
+++ main.py
@@ -18,35 +18,41 @@ class Bar:
         pygame.draw.line(self.surface, self.color, (self.pos,700), (self.pos,700-self.val), 10)
 
 class Window:
-    def __init__(self, width, height, bg_color, caption):
-        self.tableau = []
-        self.bg = bg_color
+    def __init__(self, width, height, caption):
+        self.barres = []
         self.width = width
         self.height = height
+
+        #Rectangle contenant le menu
+        self.menu = pygame.Rect(0, 0, self.width, self.height // 6)
+
+        #Boutons du menu :
+        
+        #Nom de la fenêtre
         self.caption = caption
+
+        #Création et configuration de la surface
         self.surface = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption(self.caption)
-        self.surface.fill(self.bg)
+        self.surface.fill((0,42,0))
         
     def random_bars(self):
-        """
-        Commence à 40 pixels et dessine le nombre de barres pour remplir l'écran automatiquement selon sa largeur.
-        """
+        #Commence à 40 pixels et dessine le nombre de barres pour remplir l'écran automatiquement selon sa largeur.
         position = 40
         for i in range((self.width -80) // 12):
-            self.tableau.append(Bar(random.randint(0,500),position,self.surface))
+            self.barres.append(Bar(random.randint(0,500),position,self.surface))
             position += 12
     
     def refresh(self):
         """
         Remplis l'écran de noir et redessine les barres.
         """
-        self.surface.fill(self.bg)
-        for Bar in self.tableau:
+        self.surface.fill((0,0,0))
+        pygame.draw.rect(self.surface, (230,230,230), self.menu)
+        for Bar in self.barres:
             Bar.draw()
-        pygame.display.flip()
 
-window = Window(1280, 720, (0,0,0), "AlgoViz")
+window = Window(1280, 720, "AlgoViz")
 window.random_bars()
 window.refresh()
 
@@ -61,6 +67,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
                 window.random_bars()
+                window.refresh()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if 25 <= mouse[0] <= 50 and 25 <= mouse[1] <= 50:
                 print('Button 1 pressed')
